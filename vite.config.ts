bab077cba49826564e5b0225fd61df0afc232775
Manfred Michaelis <mm@michm.de>
2024-09-02 01:14:59 +0200

    fix(formatting)

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCompression(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [viteCompression(), react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
