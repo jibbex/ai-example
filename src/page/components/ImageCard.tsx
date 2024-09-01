3cd3a0f13e1d3fd55d649f16932e96fb21c75585
Manfred Michaelis <mm@michm.de>
2024-09-01 02:53:16 +0200

    content(added not morgan freeman as an example for audio processing)

--- src/page/components/ImageCard.tsx
+++ src/page/components/ImageCard.tsx
@@ -12,6 +12,54 @@ export interface ImageCardProps {
     content?: string | ContentElement | null
 }
 
+interface ImageItemProps {
+    src?: string | null;
+    alt?: string | null;
+    style?: object | null;
+    className?: string | null;
+    key?: string | null;
+    type: TYPE;
+    content?: string | ContentElement | null
+}
+
+function ImageItem({src, alt, style, className, key, type, content}: ImageItemProps): React.JSX.Element {
+    switch (type) {
+        case TYPE.Img:
+            return (
+                <img
+                    key={key}
+                    src={src}
+                    alt={alt}
+                    style={style}
+                    className={className}
+                />
+            );
+        case TYPE.Div:
+            return (
+                <div
+                    key={key}
+                    style={style}
+                    className={className}
+                >
+                    {!!content && (content as string)}
+                </div>
+            );
+        case TYPE.Lnk:
+            return (
+                <a
+                    key={key}
+                    href={src}
+                    style={style}
+                    className={className}
+                >
+                    {!!content && (content as string)}
+                </a>
+            );
+        default:
+            return <></>;
+    }
+}
+
 /**
  * This component represents an image card with content.
  *
@@ -19,53 +67,41 @@ export interface ImageCardProps {
  * @returns {React.JSX.Element} The image card component.
  */
 export function ImageCard({ src, alt, style, className, key, type, content }: ImageCardProps): React.JSX.Element {
-    const element = React.useMemo(() => {
-        switch (type) {
-            case TYPE.Img:
-                return (
-                    <img
-                        key={key}
+    return (
+        <CardContainer className='left-0 md:left-[25vw] md:bottom-[-3em] lg:bottom-4'>
+            <CardBody
+                className="bg-gradient-to-br from-gray-100/20 via-red-400/50 to-indigo-900/20 backdrop-blur-xl relative group/card hover:shadow-2xl border-white/[0.6] min-w-[600px] w-full sm:w-[30rem] h-auto rounded-2xl p-0 border">
+                <CardItem
+                    translateY="-10"
+                    translateZ="-20"
+                    rotateZ={-4}
+                    style={style}
+                >
+                    <ImageItem
                         src={src}
                         alt={alt}
                         style={style}
                         className={className}
+                        key={key}
+                        type={type}
+                        content={content}
                     />
-                );
-            case TYPE.Div:
-                return (
-                    <div
-                        key={key}
-                        style={style}
-                        className={className}
-                    >
-                        {!!content && (content as ContentElement).content}
-                    </div>
-                );
-            case TYPE.Lnk:
-                return (
-                    <a
-                        key={key}
-                        href={src}
-                        style={style}
-                        className={className}
-                    >
-                        {!!content && (content as ContentElement).content}
-                    </a>
-                );
-            default:
-                return <></>;
-        }
-    }, [src, alt, key, type]);
-    return (
-        <CardContainer className='left-0 md:left-[25vw] md:bottom-[-3em] lg:bottom-3'>
-            <CardBody
-                className="bg-gradient-to-br from-gray-100/20 via-red-400/100 to-indigo-900/60 backdrop-blur-sm relative group/card hover:shadow-2xl dark:bg-black border-white/[0.6] min-w-[600px] w-full sm:w-[30rem] h-auto rounded-2xl p-0 border">
-                <CardItem
-                    translateZ="40"
-                    style={style}
-                >
-                    {element}
                 </CardItem>
+                {!!content && 
+                    <CardItem
+                        translateZ="30"
+                        translateY="-5"
+                        rotateZ={2}
+                        className="p-6 bg-black/20 rounded-md my-2 mx-auto"
+                        style={{ with: 'calc(100% - 0.5rem)' }}
+                    >
+                        <ImageItem 
+                            type={TYPE.Div}
+                            content={content}
+                            key={`${key}--content`}
+                        />
+                    </CardItem>
+                }
             </CardBody>
         </CardContainer>
     );
