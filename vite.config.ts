58ec4da566b91aea45d73a772be9cc0b2406c730
Manfred Michaelis <mm@michm.de>
2024-09-02 06:39:49 +0200

    removed()

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/ai-example/",
     plugins: [viteCompression(), react()],
     resolve: {
         alias: {
